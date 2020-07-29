b5622966b8106ea882d379d8ba33ac649ae262cf
Shreyas Srinivasa <58994291+sastry17@users.noreply.github.com>
2020-07-29 14:43:42 +0200

    Updated testbrot.py
    Updated testbrot.py

--- testbrot.py
+++ testbrot.py
@@ -5,3 +5,16 @@ Created on Wed Jul 29 12:43:21 2020
 @author: JG07DA
 """
 
+import unittest
+import variant
+import numpy as np
+
+
+class TestMandelBrot(unittest.TestCase):
+    
+    def testMethods(self):
+        self.methods = ['variant.mandel_set_naive(xmin,xmax,ymin,ymax,width,height,maxiter, threshold)']
+    
+    
+    def test_mandelbrot_naive(self):
+        
