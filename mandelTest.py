b0cbb60857a4b9bf8cba4a43b569e923b5b91ac1
Shreyas Srinivasa <58994291+sastry17@users.noreply.github.com>
2020-08-06 11:49:31 +0200

    Update mandeltest.py
    updated mandelTest.py

--- mandelTest.py
+++ mandelTest.py
@@ -37,22 +37,7 @@ class MandelTest(unittest.TestCase):
            y = eval(m)
            np.testing.assert_allclose(x,y)  
            
-    def test_mandel_vectorize_mp(self):
-        xmin=-2.0 # Rc lower bound
-        xmax=1.0  # Rc upper bound
-        ymin=-1.5 # Ic lower bound
-        ymax=1.5
-        width=500  #Pre
-        height=500 #Pim
-        maxiter=100 #iterations
-        threshold = 2
-        
-        x = vat.mandel_set_naive(xmin,xmax,ymin,ymax,width,height,maxiter, threshold)
-       
-        for m in self.methods:
-            y = eval(m)
-            np.testing.assert_allclose(x,y)  
-        
+
            
    
 if __name__ == '__main__':
